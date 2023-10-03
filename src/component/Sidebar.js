7a6737358fc1c397140940a51fd7832684294ae8
othmaneamrani <othmaneamrani@gmail.com>
2023-10-03 18:49:19 +0100

    searchBarre

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,13 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Sidebar(){
+
+    const [showSearch, setShowSearch] = useState(false);
+    const activeSearch = () => {
+        setShowSearch(!showSearch);
+      };
+
+
     return (
         <div className="sidebar">
         <ul className="sidebarList">
-            <li><a href="#">Créer personne</a></li>
-            <li><a href="#">Modifier personne</a></li>
-            <li><a href="#">Lister personne</a></li>
-            <li><a href="#">Supprimer personne</a></li>
+
+            <li><a onClick={activeSearch} className="sidebar-l3" href="#">Chercher personne</a>
+            {showSearch && (
+            <form className="search-form">
+              <input
+                type="text"
+                placeholder="Rechercher..."
+                className="search-input"
+              />
+              <button type="submit" className="search-button">
+                Rechercher
+              </button>
+            </form>
+          )}</li>
+
+
+            <li><a className="sidebar-l3" href="#">Créer personne</a></li>
+            <li><a className="sidebar-l3" href="#">Modifier personne</a></li>
+            <li><a className="supprimer" href="#">Supprimer personne</a></li>
 
         </ul>
     </div>
